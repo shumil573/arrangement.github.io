2c18f450be18b05b4f0c3201dbaf087962c3c3e5
shumil573 <heshuyao@bupt.edu.cn>
2021-05-17 22:47:03 +0800

    对优化移动端提示的尝试

--- src/index.js
+++ src/index.js
@@ -175,21 +175,21 @@ class TimeDOM extends React.Component {
         const old_hours = Math.floor(sum / 3600);
         const old_mins = Math.floor(sum / 60);
         const old_ss = sum % 60;
-        if(window.Notification && Notification.permission !== "denied") {
-          Notification.requestPermission(function(status) {
-            //var n = new Notification('通知标题', { body: '这里是通知内容！' }); 
-            var n = new Notification('时间到！', { body: old_mins+'分'+ old_ss+'秒计时结束'}); 
-          });
-        } else {
+        // if(window.Notification && Notification.permission !== "denied") {
+        //   Notification.requestPermission(function(status) {
+        //     //var n = new Notification('通知标题', { body: '这里是通知内容！' }); 
+        //     var n = new Notification('时间到！', { body: old_mins+'分'+ old_ss+'秒计时结束'}); 
+        //   });
+        // } else 
+        {
           Toast.fail("Toast！该浏览器不允许通知!"+old_mins+"分"+ old_ss+"秒计时时间到", 2);
           message.error("Error！该浏览器不允许通知!"+old_mins+"分"+ old_ss+"秒计时时间到");
-          console.alert("Alert！该浏览器不允许通知!"+old_mins+"分"+ old_ss+"秒计时时间到");
         }
         this.setState({
           counting: false,
           cached: true,
           old_hours,old_mins,old_ss
-        })
+        });
       }
     }
   }
